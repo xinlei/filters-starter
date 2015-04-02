21651f6c9bdffee97ed3be78bdb304388dab296a
xinlei <wang.xinlei15@gmail.com>
2015-04-01 18:20:46 -0700

    first commit

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
  * pixels -- an array of pixel values
  */
 function filterGrayscale(pixels) {
+
 }
 
 /* Brightens the given pixels.
@@ -12,6 +13,16 @@ function filterGrayscale(pixels) {
  * pixels -- an array of pixel values
  */
 function filterBrighten(pixels) {
+  for(var i = 0; i < pixels.length; i+= 4){
+    var red = pixels[i];
+    var green = pixels[i + 1];
+    var blue = pixels[i + 2];
+
+    pixels[i] = red + 20;
+    pixels[i + 1] = green + 20;
+    pixels[i + 2] = blue + 20;
+  }
+  return pixels;
 }
 
 /* Applies a threshold filter to the given pixels. Makes all pixels above
